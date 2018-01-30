1c7b0b3ff8b8a205e5892ce841a58e44f3987c3d
zhang227 <zhang.jinf@husky.neu.edu>
2018-01-29 21:38:29 -0600

    splite navigation bar

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,21 +1,17 @@
 import React, { Component } from 'react';
-import logo from '../Assets/Images/logo.svg';
-import '../Style/App.css';
+import { TopNavBar } from './TopNavBar';
+import { Main } from './Main';
+import '../styles/App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <TopNavBar/>
+                <Main/>
+            </div>
+        );
+    }
 }
 
 export default App;
